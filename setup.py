da5c824942b17e6902a922b723dad6bfa1f489a1
DS-FAU <42653001+ds-fau-ck@users.noreply.github.com>
2024-04-18 07:49:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-AUTHOR_USER_NAME = "Kulakirti Chakma"
+AUTHOR_USER_NAME = "KC"
 SRC_REPO = "Sensor"
 LIST_OF_REQUIREMENTS = []
 
@@ -7,7 +7,6 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    author_email="kirticse.chakma869@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
